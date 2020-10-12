3f0c460988ac3a1ad26b9fec89b2cc76312f8081
FC122 <filip.cica1234@gmail.com>
2020-10-12 11:30:39 +0200

    Changed code

--- Program.cs
+++ Program.cs
@@ -6,10 +6,7 @@ namespace Proba
     {
         static void Main(string[] args)
         {
-            Dog Rex=new Dog();
-            int age;
-            Rex.SetAge(Convert.ToInt32(Console.ReadLine()));
-            Console.WriteLine(Rex.GetAge());
+          
             
 
         }
