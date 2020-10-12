b2ddb0e391fc0b195911603ee7ddab5a1a6b516e
FC122 <filip.cica1234@gmail.com>
2020-10-12 11:24:11 +0200

    My first program

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Proba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog Rex=new Dog();
+            int age;
+            Rex.SetAge(Convert.ToInt32(Console.ReadLine()));
+            Console.WriteLine(Rex.GetAge());
+            
+
+        }
+    }
+}
